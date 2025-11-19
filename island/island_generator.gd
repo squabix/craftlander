e9a5cda907f150b1c80f4b1ce2438448b6aa4cf9
Squabix <102375958+squabix@users.noreply.github.com>
2025-11-19 14:42:03 -0700

    Add support for island generation with multiple noise textures

--- island/island_generator.gd
+++ island/island_generator.gd
@@ -6,7 +6,7 @@ var update_texture_action: Callable = update_shader_texture
 
 @export var image_width: int
 @export var image_height: int
-@export var noise_texture: Texture2D
+@export var noise_textures: Array[Texture2D]
 @export var taper_gradient_texture: GradientTexture2D
 @export var taper_gradient_strength: float = 0.9
 @export var absolute_gradient_texture: GradientTexture2D
@@ -14,18 +14,22 @@ var update_texture_action: Callable = update_shader_texture
 @export var collision_shape: CollisionShape3D
 @export var heightmap_rect: TextureRect
 
+func shader_set(parameter: String, to: Variant) -> void:
+	mesh_instance.material_override.set_shader_parameter(
+		parameter,
+		to
+	)
+
 func _ready() -> void:
 	await get_tree().process_frame
 	collision_shape.shape = HeightMapShape3D.new()
-	var image_texture: ImageTexture = update_shader_texture()
+	var image_texture: ImageTexture = generate_image_texture()
+	
 	heightmap_rect.texture = image_texture
 
 func update_shader_texture() -> ImageTexture:
 	var image_texture: ImageTexture = generate_image_texture()
-	mesh_instance.material_override.set_shader_parameter(
-		"heightmap",
-		image_texture
-	)
+	shader_set("heightmap", image_texture)
 	#mesh_instance.material_override.set_shader_parameter(
 		#"albedo_texture",
 		#image_texture
@@ -35,18 +39,31 @@ func update_shader_texture() -> ImageTexture:
 func get_pixel(x: int, y: int, texture_images: Dictionary[Texture2D, Image]) -> Color:
 	var sample: Callable = func(texture: Texture2D) -> float: return texture_images[texture].get_pixel(x, y).r
 	
-	var value: float = sample.call(noise_texture)
+	var value: float = 0.0
+	
+	for texture in noise_textures:
+		value += sample.call(texture)
+	value /= float(len(noise_textures))
+	
 	value -= (1.0 - sample.call(taper_gradient_texture)) * taper_gradient_strength
 	value = min(value, sample.call(absolute_gradient_texture))
 	
 	return Color(value, value, value)
 
-func generate_image_texture() -> ImageTexture:
-	var texture_images: Dictionary[Texture2D, Image]
+func load_image_textures() -> Dictionary[Texture2D, Image]:
+	var textures: Array[Texture2D] = [
+		taper_gradient_texture,
+		absolute_gradient_texture
+	]
+	textures.append_array(noise_textures)
 	
-	texture_images[noise_texture] = noise_texture.get_image()
-	texture_images[taper_gradient_texture] = taper_gradient_texture.get_image()
-	texture_images[absolute_gradient_texture] = absolute_gradient_texture.get_image()
+	var loaded_texture_images: Dictionary[Texture2D, Image]
+	for texture in textures:
+		loaded_texture_images[texture] = texture.get_image()
+	return loaded_texture_images
+
+func generate_image_texture() -> ImageTexture:
+	var texture_images: Dictionary[Texture2D, Image] = load_image_textures()
 	
 	# Resize all images to same size
 	for image in texture_images.values():
