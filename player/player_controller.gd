264f7720978e3dce954222a6f8d1342769722b4e
Squabix <102375958+squabix@users.noreply.github.com>
2025-12-20 20:05:07 -0700

    Revert "Nit"
    This reverts commit fe40d67b0fd6e6a78bcdd0967a417e3c69e3c475.

--- player/player_controller.gd
+++ player/player_controller.gd
@@ -1,20 +1,12 @@
 extends Controller3D
 
+const HEAD_COLLISION_HEIGHT_MARGIN := 0.25
+const HEAD_HEIGHT_LERP_SPEED := 0.3
+
 const MOUSE_SENSITIVITY := 0.35
 
-const ACTION_MOVE_LEFT := "move_left"
-const ACTION_MOVE_RIGHT := "move_right"
-const ACTION_MOVE_FORWARD := "move_forward"
-const ACTION_MOVE_BACKWARD := "move_backward"
 
-const ACTION_JUMP := "jump"
-const ACTION_TOGGLE_MOUSE_CAPTURE := "ui_cancel"
-const ACTION_INTERACT := "interact"
-const ACTION_USE_PRIMARY := "use_primary"
-
-const ACTION_SCROLL_UP := "scroll_up"
-const ACTION_SCROLL_DOWN := "scroll_down"
-
+var crouch_speed := 0.2
 
 @export var head: Node3D
 @export var current_collision_shape: CollisionShape3D
@@ -27,23 +19,30 @@ func turn_head(relative: Vector2) -> void:
 	entity.rotate_vertical(-relative.y * MOUSE_SENSITIVITY)
 	entity.rotate_horizontal(-relative.x * MOUSE_SENSITIVITY)
 
-func get_motion_vector() -> Vector2:
-	return Input.get_vector(
+func update(_delta: float) -> void:
+	adjust_head_to_collision()
+	
+	# Move
+	entity.move_planar(
+		Input.get_vector(
 			"move_left",
 			"move_right",
 			"move_backward",
 			"move_forward"
-		) 
-
-func update(_delta: float) -> void:
-	
-	# Move
-	entity.move_planar(
-		get_motion_vector().normalized()
+		).normalized()
 	)
 	
-	if Input.is_action_just_pressed(ACTION_JUMP):
+	if Input.is_action_just_pressed("jump"):
 		entity.jump()
+
+func adjust_head_to_collision() -> void:
+	if not current_collision_shape:
+		return
+	head.position.y = lerp(
+		head.position.y,
+		current_collision_shape.shape.height - HEAD_COLLISION_HEIGHT_MARGIN,
+		HEAD_HEIGHT_LERP_SPEED
+	)
 
 func _input(event: InputEvent) -> void:
 	if not is_controlling():
@@ -53,23 +52,16 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		turn_head(event.relative * MOUSE_SENSITIVITY)
 	
-	if event.is_action_pressed(ACTION_TOGGLE_MOUSE_CAPTURE):
+	if event.is_action_pressed("ui_cancel"):
 		MouseModeController.toggle(Input.MOUSE_MODE_CAPTURED, Input.MOUSE_MODE_VISIBLE)
-	
-	if event.is_action_pressed(ACTION_INTERACT):
-		entity.interact()
-	
-	if event.is_action_pressed(ACTION_SCROLL_UP):
-		entity.inventory_holder_link.scroll(-1)
-	
-	elif event.is_action_pressed(ACTION_SCROLL_DOWN):
-		entity.inventory_holder_link.scroll(1)
-	
-	if Input.is_action_pressed(ACTION_USE_PRIMARY):
-		entity.use_item()
 
 func get_accel_dir() -> Vector3:
 	return Util.vec2to3(
-		get_motion_vector(),
+		Input.get_vector(
+			"move_left",
+			"move_right",
+			"move_forward",
+			"move_backward"
+		),
 		Util.VECTOR3Y
 	)
