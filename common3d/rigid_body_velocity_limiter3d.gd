3a0ac83e329a269c4252b0aed7adb2bb23adb552
Squabix <102375958+squabix@users.noreply.github.com>
2025-12-27 19:57:09 -0700

    Add bool vector 3 support to velocity limiter

--- common3d/rigid_body_velocity_limiter3d.gd
+++ common3d/rigid_body_velocity_limiter3d.gd
@@ -1,11 +1,19 @@
 extends Node
 class_name RigidBodyVelocityLimiter3D
 
+
 @export var limit: float = 5.0
+@export var axes: BoolVector3
 
 func _process(delta: float) -> void:
-	if not get_parent() is RigidBody3D:
+	var parent := get_parent()
+	if not parent is RigidBody3D:
 		Util.safe_free(self)
 		return
-	if get_parent().linear_velocity.length() > limit:
-		get_parent().linear_velocity = get_parent().linear_velocity.limit_length(limit)
+	var original_velocity: Vector3 = parent.linear_velocity
+	if original_velocity.length() > limit:
+		parent.linear_velocity = BoolVector3.replace(
+			original_velocity,
+			original_velocity.limit_length(limit),
+			axes
+		)
