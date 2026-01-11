755fd32caf840638fc6d71f7e473ed13abe17d03
Squabix <102375958+squabix@users.noreply.github.com>
2026-01-11 15:41:50 -0700

    Nit

--- main/test.gd
+++ main/test.gd
@@ -1,5 +1,9 @@
 extends Node3D
 
+func _ready() -> void:
+	await get_tree().process_frame
+	EventBus.trigger("island_populated")
+
 func _process(delta: float) -> void:
 	pass
 	#if Input.is_action_pressed("move_right"):
