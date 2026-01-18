95c01ab43768b222f577cbee08075fb5e4c7a2ce
Squabix <102375958+squabix@users.noreply.github.com>
2026-01-18 16:38:37 -0700

    Add hiding rootling when off screen
    Introduce a VisibleOnScreenNotifier3D and related nodes to the Rootling scene. The controller now shows or hides the entity based on its on-screen status, improving performance.

--- entities/enemies/rootling/rootling_controller.gd
+++ entities/enemies/rootling/rootling_controller.gd
@@ -3,12 +3,21 @@ extends Controller3D
 const MIN_TARGET_DISTANCE := 2.0
 
 @export var sight: RadialSight3D
+@export var on_screen_notifier: VisibleOnScreenNotifier3D
 
 @onready var guide: NavEntityGuide3D = $NavEntityGuide3D
 
 func _process(_delta: float) -> void:
+	if on_screen_notifier.is_on_screen():
+		entity.show()
+	else:
+		entity.hide()
+		if not sight.does_see_target():
+			return
+		
 	if not sight.does_see_target():
 		return
+	
 	guide.face_target(sight.target_position)
 	if entity.global_position.distance_to(sight.target.global_position) > MIN_TARGET_DISTANCE:
 		entity.move_forward()
