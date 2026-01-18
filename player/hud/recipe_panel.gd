284bb7d5c86e11a76a8cf2e9b0e32f58157f4b7b
Squabix <102375958+squabix@users.noreply.github.com>
2026-01-18 13:32:27 -0700

    Add recipe panel UI
    Add a new RecipePanel UI to the crafting menu, including a grid display and scrollable recipe list. Update player.tscn to integrate the new panel and assets.

---
+++ player/hud/recipe_panel.gd
@@ -0,0 +1,32 @@
+extends Control
+
+const LAYOUT_OFFSET := Vector2i(2, -3)
+
+@onready var recipe_list_box: VBoxContainer = $RecipeList/RecipeListBox
+@onready var recipe_entry_template: Button = $RecipeList/RecipeListBox/RecipeEntry
+
+@onready var grid_container: VBoxContainer = $RecipeDisplay/GridContainer
+
+func _ready() -> void:
+	recipe_entry_template.hide()
+	for recipe in RecipeBook.all_recipes:
+		var new_entry: Button = recipe_entry_template.duplicate()
+		recipe_list_box.add_child(new_entry)
+		new_entry.show()
+		new_entry.text = recipe.result.item.name
+		new_entry.get_node("IconRect").texture = recipe.result.item.icon
+		new_entry.pressed.connect(display_recipe.bind(recipe))
+
+func clear_recipe_display() -> void:
+	for row in grid_container.get_children():
+		for label in row.get_children():
+			label.text = ""
+
+func display_recipe(recipe: ItemRecipe) -> void:
+	clear_recipe_display()
+	for item_position in recipe.layout:
+		get_grid_item_label(item_position).text = recipe.layout[item_position].name
+
+func get_grid_item_label(label_position: Vector2i) -> Label:
+	var row := grid_container.get_child(-label_position.y + LAYOUT_OFFSET.y)
+	return row.get_child(label_position.x + LAYOUT_OFFSET.x)
