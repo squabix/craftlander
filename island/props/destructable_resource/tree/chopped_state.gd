c8f5768ad6acc3499b75eecf5c8ea6b479ee8a05
Squabix <102375958+squabix@users.noreply.github.com>
2026-01-19 12:25:39 -0700

    Remove tree workbench interactable feature
    Delete the tree_workbench_interactable.gd script and its references from the tree scene.

--- island/props/destructable_resource/tree/chopped_state.gd
+++ island/props/destructable_resource/tree/chopped_state.gd
@@ -8,5 +8,4 @@ func enter() -> void:
 	root.chop_particles.show()
 	root.chop_particles.global_transform = root.hurtbox.get_child(0).global_transform # Transform particles to center of tree
 	root.chop_particles.emitting = true
-	%BenchInteractable.enable()
 	#%Spawner3D.drop()
