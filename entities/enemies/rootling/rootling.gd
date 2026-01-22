bdc7a719064a310bfdff1386d5de7853e267930a
Squabix <102375958+squabix@users.noreply.github.com>
2026-01-21 20:09:35 -0700

    Add animation switching to rootling based on speed

--- entities/enemies/rootling/rootling.gd
+++ entities/enemies/rootling/rootling.gd
@@ -5,12 +5,16 @@ const ANIM_WALK = "walk"
 const ANIM_IDLE = "idle"
 const ANIM_ATTACK = "attack"
 
-const RUN_THRESHOLD = 1.0
+const RUN_THRESHOLD = 2.5
+const WALK_THESHOLD = 0.3
 
 @export var animation_player: AnimationPlayer
 
-func _process(delta: float) -> void:
-	if Util.vec3to2(velocity, Util.VECTOR3Y).length() > RUN_THRESHOLD:
+func _process(_delta: float) -> void:
+	var velocity_length := Util.vec3to2(velocity, Util.VECTOR3Y).length()
+	if velocity_length > RUN_THRESHOLD:
 		animation_player.play("run")
+	elif velocity_length > WALK_THESHOLD:
+		animation_player.play("walk")
 	else:
 		animation_player.play("idle")
