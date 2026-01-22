5e0457d6282b45c8399be7bb2eb69ea40b50a1ec
Squabix <102375958+squabix@users.noreply.github.com>
2026-01-21 20:21:37 -0700

    Disable rootling walk animation
    Disable animation because animaton interpolation system is not fully implemented yet

--- entities/enemies/rootling/rootling.gd
+++ entities/enemies/rootling/rootling.gd
@@ -5,7 +5,7 @@ const ANIM_WALK = "walk"
 const ANIM_IDLE = "idle"
 const ANIM_ATTACK = "attack"
 
-const RUN_THRESHOLD = 2.5
+const RUN_THRESHOLD = 0.5
 const WALK_THESHOLD = 0.3
 
 @export var animation_player: AnimationPlayer
@@ -14,7 +14,7 @@ func _process(_delta: float) -> void:
 	var velocity_length := Util.vec3to2(velocity, Util.VECTOR3Y).length()
 	if velocity_length > RUN_THRESHOLD:
 		animation_player.play("run")
-	elif velocity_length > WALK_THESHOLD:
-		animation_player.play("walk")
+	#elif velocity_length > WALK_THESHOLD:
+		#animation_player.play("walk")
 	else:
 		animation_player.play("idle")
