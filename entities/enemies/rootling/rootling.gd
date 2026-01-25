0538a3d46bf7843e9330ce5752b34e9331749d06
Squabix <102375958+squabix@users.noreply.github.com>
2026-01-24 20:59:28 -0700

    Update rootling constants to use inferred typing

--- entities/enemies/rootling/rootling.gd
+++ entities/enemies/rootling/rootling.gd
@@ -1,12 +1,12 @@
 extends Entity3D
 
-const ANIM_RUN = "run"
-const ANIM_WALK = "walk"
-const ANIM_IDLE = "idle"
-const ANIM_ATTACK = "attack"
+const ANIM_RUN := "run"
+const ANIM_WALK := "walk"
+const ANIM_IDLE := "idle"
+const ANIM_ATTACK := "attack"
 
-const RUN_THRESHOLD = 0.5
-const WALK_THESHOLD = 0.3
+const RUN_THRESHOLD := 0.5
+const WALK_THESHOLD := 0.3
 
 @export var anim_player: AnimationPlayer
 @export var anim_tree: AnimationTree
