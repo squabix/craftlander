000a6e3386f84c1c1cc22d699a66012bbf330711
Squabix <102375958+squabix@users.noreply.github.com>
2026-01-24 20:57:29 -0700

    Add rootling anim interpolation via anim tree

--- entities/enemies/rootling/rootling.gd
+++ entities/enemies/rootling/rootling.gd
@@ -9,12 +9,13 @@ const RUN_THRESHOLD = 0.5
 const WALK_THESHOLD = 0.3
 
 @export var anim_player: AnimationPlayer
+@export var anim_tree: AnimationTree
 
 func _process(_delta: float) -> void:
+	
+	# Interpolate between animations by velocity
 	var velocity_length := Util.vec3to2(velocity, Util.VECTOR3Y).length()
-	if velocity_length > RUN_THRESHOLD:
-		anim_player.play("run")
-	#elif velocity_length > WALK_THESHOLD:
-		#anim_player.play("walk")
-	else:
-		anim_player.play("idle")
+	anim_tree.set(
+		"parameters/RunBlendSpace/blend_position",
+		velocity_length / movement_mode.max_speed.x
+	)
