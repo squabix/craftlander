b5fea52fb082d6c980dd57af58afe79975537bc0
Squabix <102375958+squabix@users.noreply.github.com>
2026-03-01 14:59:33 -0700

    Rename entity controller variables

--- common3d/brain/controller.gd
+++ common3d/brain/controller.gd
@@ -3,13 +3,13 @@ class_name Controller3D
 
 @export var entity: Entity3D
 
-static var _entity_brains: Dictionary
+static var _entity_controller_map: Dictionary
 
 @onready var original_entity: Entity3D = entity
 
 static func get_controller(of: Entity3D) -> Controller3D:
-	if of in _entity_brains:
-		return _entity_brains[of]
+	if of in _entity_controller_map:
+		return _entity_controller_map[of]
 	return null
 
 func is_controlling() -> bool:
@@ -19,7 +19,7 @@ func _ready() -> void:
 	entity = original_entity
 
 func _enter_tree() -> void:
-	_entity_brains[entity] = self
+	_entity_controller_map[entity] = self
 
 func _process(delta: float) -> void:
 	if entity == null:
