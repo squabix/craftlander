fc057b116ab51a52ed23da747cd6033a4c81ab96
Squabix <102375958+squabix@users.noreply.github.com>
2026-03-01 15:00:22 -0700

    Rewrite entity vehicle seat script

--- common3d/entity/vehicle/entity_vehicle_seat3d.gd
+++ common3d/entity/vehicle/entity_vehicle_seat3d.gd
@@ -3,93 +3,36 @@ class_name Seat3D
 
 @export var vehicle: EntityVehicle3D
 @export var is_driver: bool = true
-@export var whitelisted_groups: Array[String]
-#
-#class SeatedInstance:
-	#var entity: Entity3D
-	#var brain: Brain3D
-	#var previous_parent: Node
-	#var disabled_colliders: Array[Node]
-	#
-	#func _init(seat: Seat3D, seated_entity: Entity3D) -> void:
-		#entity = seated_entity
-		#if entity == null:
-			#return
-		#
-		#previous_parent = entity.get_parent()
-		#entity.reparent(seat)
-		#Util.reset_local_transform_3d(entity)
-		#entity.set_physics_process(false)
-		#disabled_colliders = Util.disable_all_colliders(entity)
-		#
-		#brain = Brain3D.get_brain(entity)
-		#brain.entity = seat.vehicle
-	#
-	#func revert() -> void:
-		#brain.entity = entity
-		#entity.reparent(previous_parent)
-		#entity.set_physics_process(true)
-		#for collider in disabled_colliders:
-			#collider.set_deferred("disabled", false)
-#
-#var current_instance: SeatedInstance
-#
-#func is_open() -> bool:
-	#return current_instance == null
-#
-#func dismount(entity: Entity3D) -> void:
-	#if current_instance == null:
-		#return
-	#
-	#if entity != current_instance.entity:
-		#return
-	#
-	#if not is_instance_valid(entity):
-		#current_instance = null
-		#return
-	#
-	## TODO: Copy position AND rotation of dismount area
-	#var dismount_position: Vector3 = get_dismount_position()
-	#if dismount_position != global_position:
-		#current_instance.entity.global_position = dismount_position
-	#
-	#current_instance.revert()
-	#current_instance = null
-#
-#func get_dismount_position() -> Vector3:
-	#for dismount_area in vehicle.dismount_areas:
-		##if not dismount_area.has_overlapping_bodies():
-		#return dismount_area.global_position
-	#return global_position
-#
-#func is_whitelisted(entity: Entity3D) -> bool:
-	#return true
-	#if whitelisted_groups.is_empty():
-		#return true
-	#for group in whitelisted_groups:
-		#if entity.is_in_group(group):
-			#return true
-	#return false
-#
-#func can_mount(entity: Entity3D) -> bool:
-	#return is_whitelisted(entity)
-#
-#func get_dismount_signals(of: Entity3D) -> Array[Signal]:
-	#return [of.tree_exiting, of.dismount_requested]
-#
-#func mount(entity: Entity3D) -> bool:
-	#if current_instance != null:
-		#return false
-	#
-	#if not is_whitelisted(entity):
-		#return false
-	#
-	#current_instance = SeatedInstance.new(self, entity)
-	#
-	#var bound_dismount: Callable = dismount.bind(entity)
-	#entity.tree_exiting.connect(bound_dismount)
-	#entity.dismount_requested.connect(bound_dismount)
-	#
-	#print(entity, " mounted ", vehicle.name, " on ", name)
-	#
-	#return true
+@export var initial_entity: Entity3D
+
+var mounted_entity: Entity3D
+var mounted_controller: Controller3D
+
+func _ready() -> void:
+	if initial_entity != null:
+		mount(initial_entity)
+
+func _process(delta: float) -> void:
+	if is_instance_valid(mounted_entity):
+		mounted_entity.global_position = global_position
+
+func mount(entity: Entity3D) -> bool:
+	
+	# Cannot mount new entity when already mounted
+	if is_instance_valid(mounted_entity):
+		return false
+	
+	mounted_entity = entity
+	for child in mounted_entity.get_children():
+		print(Util.get_object_class(child))
+	mounted_controller = Controller3D.get_controller(mounted_entity)
+	if mounted_controller:
+		mounted_controller.entity = vehicle
+	
+	return true
+
+func dismount() -> void:
+	if is_instance_valid(mounted_entity):
+		mounted_controller.entity = mounted_entity
+	mounted_entity = null
+	mounted_controller = null
