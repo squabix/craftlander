3e370db03c53d1b512a83e5a69700beb2eda8f33
Squabix <102375958+squabix@users.noreply.github.com>
2026-03-01 20:46:44 -0700

    Update entity controller to extend state machine

--- common3d/controller/entity_controller3d.gd
+++ common3d/controller/entity_controller3d.gd
@@ -1,16 +1,25 @@
-extends Node
+extends StateMachine
 class_name EntityController3D
 
 signal updated_entity(to: Entity3D)
 
-@export var entity: Entity3D:
-	set(to):
-		entity = to
-		updated_entity.emit(to)
+@export var entity_type_state_map: Dictionary[String, String]
+@export var initial_entity: Entity3D
+
+var entity: Entity3D
+
+func update_entity(to: Entity3D) -> void:
+	entity = to
+	for child in get_children():
+		if child is State:
+			child.root = entity
+	
+	if is_instance_valid(entity) and entity.type in entity_type_state_map:
+		enter_state(entity_type_state_map[entity.type])
+	
+	updated_entity.emit(to)
 
 static var _entity_controller_map: Dictionary[Entity3D, EntityController3D]
-
-@onready var original_entity: Entity3D = entity
 
 static func get_controller(of: Entity3D) -> EntityController3D:
 	if of in _entity_controller_map:
@@ -21,15 +30,8 @@ func is_controlling() -> bool:
 	return is_instance_valid(entity)
 
 func _ready() -> void:
-	entity = original_entity
-
-func _enter_tree() -> void:
+	if root == null:
+		root = initial_entity
+	super()
+	update_entity(initial_entity)
 	_entity_controller_map[entity] = self
-
-func _process(delta: float) -> void:
-	if entity == null:
-		return
-	update(delta)
-
-func update(_delta: float) -> void:
-	pass
