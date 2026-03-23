c2627ee58d70fc071e193d026706e96fe208266e
Squabix <102375958+squabix@users.noreply.github.com>
2026-03-22 19:59:32 -0600

    Add quitting to title from death

--- main/main.gd
+++ main/main.gd
@@ -9,10 +9,23 @@ var title_screen: TitleScreen
 var level: Node3D
 
 func _ready() -> void:
-	title_screen = $TitleScreen
-	title_screen.started_new_game.connect(start_new_game)
+	load_title()
+	EventBus.subscribe("quit_to_title", quit_to_title)
 
 func start_new_game(save: int) -> void:
 	title_screen.queue_free()
 	level = island_scenes[save].instantiate()
 	add_child(level)
+
+func quit_level() -> void:
+	level.queue_free()
+	level = null
+
+func quit_to_title() -> void:
+	quit_level()
+	load_title()
+
+func load_title() -> void:
+	title_screen = title_screen_scene.instantiate()
+	add_child(title_screen)
+	title_screen.started_new_game.connect(start_new_game)
--- player/hud/player_death_screen.gd
+++ player/hud/player_death_screen.gd
@@ -1,6 +1,11 @@
 extends Control
 
+@onready var respawn_button: Button = $OptionsContainer/RespawnButton
+@onready var quit_button: Button = $OptionsContainer/QuitButton
+
 func _ready() -> void:
 	hide()
 	%Health.died.connect(show)
 	%Health.revived.connect(hide)
+	
+	quit_button.pressed.connect(EventBus.trigger.bind("quit_to_title"))
