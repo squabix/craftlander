61f6543f957fdef39f051bc923ab30dbd85dc1de
Squabix <102375958+squabix@users.noreply.github.com>
2026-04-22 14:07:15 -0600

    Add dedicated island script

---
+++ main/island.gd
@@ -0,0 +1,16 @@
+extends Node3D
+
+const ISLAND_CENTER_SPAWN_HEIGHT := 55.0
+
+enum PlayerSpawnMode {BOAT, ISLAND_CENTER}
+
+@export var player: Player
+@export var boat_driver_seat: Seat3D
+@export var current_player_spawn_mode := PlayerSpawnMode.BOAT
+
+func _ready() -> void:
+	match current_player_spawn_mode:
+		PlayerSpawnMode.BOAT:
+			boat_driver_seat.initial_entity = player
+		PlayerSpawnMode.ISLAND_CENTER:
+			player.global_position = Vector3(0.0, ISLAND_CENTER_SPAWN_HEIGHT, 0.0)
