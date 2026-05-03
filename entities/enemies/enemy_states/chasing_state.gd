94b3a4d96c796ee85a81b70fd77361bb5637e5c2
Squabix <102375958+squabix@users.noreply.github.com>
2026-05-03 09:08:10 -0600

    Add target losing option to chasing state

--- entities/enemies/enemy_states/chasing_state.gd
+++ entities/enemies/enemy_states/chasing_state.gd
@@ -4,18 +4,22 @@ extends State
 @export var sight: RadialSight3D
 @export var min_approach_distance := 1.5
 
+@export var can_lose_target := true
+@export var lose_target_state := ""
+
 @onready var inventory: Inventory = %Inventory
 @onready var item_holder: ItemHolder3D = %ItemHolder3D
 
 func update(_delta: float) -> void:
-	if sight.does_see_target():
-		nav_guide.set_target(sight.target_position)
-		nav_guide.face_target()
-		
-		if nav_guide.nav.distance_to_target() > min_approach_distance:
-			if nav_guide.nav.is_target_reachable():
-				nav_guide.entity.move_forward()
-		else:
-			item_holder.use_item()
+	if can_lose_target and (sight != null and not sight.does_see_target()):
+		transition_to(lose_target_state)
+		return
+	
+	nav_guide.set_target(sight.target_position)
+	nav_guide.face_target()
+	
+	if nav_guide.nav.distance_to_target() > min_approach_distance:
+		if nav_guide.nav.is_target_reachable():
+			nav_guide.entity.move_forward()
 	else:
-		transition_to("Wandering")
+		item_holder.use_item()
