387fc0534eea9d12bb3adc9021a9dbcd27906924
Squabix <102375958+squabix@users.noreply.github.com>
2026-05-17 23:42:28 -0600

    Boat states nit

--- boat/boat_docked_state.gd
+++ boat/boat_docked_state.gd
@@ -1,6 +1,10 @@
 extends State
 
+@export var driver_seat: Seat3D
+
 func enter() -> void:
-	var dismounted_player: Player = %DriverSeat.dismount()
-	if dismounted_player != null:
-		dismounted_player.respawn_point_node = %DriverSeat
+	var dismounted_player: Player = driver_seat.dismount()
+	if dismounted_player == null:
+		return
+	
+	dismounted_player.respawn_point_node = driver_seat
--- boat/boat_docking_state.gd
+++ boat/boat_docking_state.gd
@@ -5,7 +5,7 @@ const SUCCESS_DISTANCE := 0.5
 func enter() -> void:
 	root.driver_seat.is_driver = false
 
-func update(delta: float) -> void:
+func update(_delta: float) -> void:
 	root.move_forward()
 	if root.global_position.distance_to(root.dock_position) < SUCCESS_DISTANCE:
 		transition_to("Docked")
