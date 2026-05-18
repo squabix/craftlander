5ff743c575ad35dad0ebd6df6ab2fb5afd345227
Squabix <102375958+squabix@users.noreply.github.com>
2026-05-18 00:19:43 -0600

    Restructure destructable resource
    Split bloated ready function into several smaller functions

--- island/props/destructable_resource/destructable_resource.gd
+++ island/props/destructable_resource/destructable_resource.gd
@@ -8,22 +8,34 @@ class_name DestructableResource
 @export var inventory: Inventory
 @export var health: Health
 
+var damage_source_inventories: Dictionary[Node, Inventory] = {}
+
+func randomize_health() -> void:
+	if not is_instance_valid(health):
+		return
+	health.hp = float(randi_range(min_hp, max_hp))
+
+func connect_hurtbox_damage() -> void:
+	var give_to_source := func(damage: Damage):
+		give_random_item(get_damage_source_inventory(damage.source))
+	hurtbox.was_dealt_damage.connect(give_to_source)
+
+func get_damage_source_inventory(source: Node) -> Inventory:
+	return Util.find_stored_child_of_class(damage_source_inventories, source, "Inventory")
+
+func give_random_item(to: Inventory) -> void:
+	var item := inventory.get_item(inventory.get_random_index_weighted())
+	inventory.give_item(item, 1, to)
+
 func _ready() -> void:
-	if health != null:
-		health.hp = float(randi_range(min_hp, max_hp))
+	randomize_health()
+	
+	if not Util.are_instances_valid([inventory, hurtbox]):
+		return
 	
 	# Once island is populated, connect hurtbox damage signal to give first inventory item to damager
-	if hurtbox != null and inventory != null:
-		EventBus.subscribe(
-			"island_populated",
-			func() -> void:
-				Util.find_child_of_class(self, "Hurtbox3D").was_dealt_damage.connect(
-					func(damage: Damage) -> void:
-						inventory.give_item(
-							inventory.get_item(inventory.get_random_index_weighted()),
-							1,
-							Util.find_child_of_class(damage.source, "Inventory")
-						)
-				),
-			tree_exiting
-		)
+	EventBus.subscribe(
+		"island_populated",
+		connect_hurtbox_damage,
+		tree_exiting
+	)
