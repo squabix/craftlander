d948da3a9d0c4fc4270adb97fa33d0a1bbf6f83d
Squabix <102375958+squabix@users.noreply.github.com>
2026-05-20 21:54:57 -0600

    Fix event bus

--- common/event_bus.gd
+++ common/event_bus.gd
@@ -1,32 +1,47 @@
 class_name EventBus
 extends Object
 
-static var subscribed_events: Dictionary = {}
+static var subscribed_events: Dictionary[String, Array] = {}
 
 static func subscribe(to: String, subscriber: Callable, unsubscribe_signal: Signal = Signal()) -> bool:
-	if subscriber.is_valid():
-		if to in subscribed_events and subscribed_events[to] is Array:
-			subscribed_events[to].append(subscriber)
-		else:
-			subscribed_events[to] = [subscriber]
-		
-		# Unsubscribe when received signal
-		unsubscribe_signal.connect(EventBus.unsubscribe.bind(to, subscriber))
-		return true
+	if not subscriber.is_valid():
+		printerr("Invalid Callable cannot subscribe to EventBus")
+		return false
 	
-	printerr("Invalid Callable cannot subscribe to EventBus")
-	return false
+	# Initialize the array for this event if it doesn't exist
+	initialize_event(to)
+	
+	# Prevent duplicate subscriptions
+	if subscriber in subscribed_events[to]:
+		return false
+	
+	subscribed_events[to].append(subscriber)
+	
+	# Unsubscribe when received signal
+	if unsubscribe_signal.get_object() != null:
+		unsubscribe_signal.connect(EventBus.unsubscribe.bind(to, subscriber), CONNECT_ONE_SHOT)
+	return true
+
+static func initialize_event(event: String) -> void:
+	if not subscribed_events.has(event):
+		subscribed_events[event] = []
 
 static func trigger(event: String, etc: Variant = null) -> bool:
-	if event in subscribed_events:
-		for subscriber in subscribed_events[event]:
-			if subscriber.is_valid():
-				if etc == null:
-					subscriber.call()
-				else:
-					subscriber.call(etc)
-		return true
-	return false
+	
+	# Event is not subscribed to
+	if not event in subscribed_events:
+		return false
+	
+	for subscriber in subscribed_events[event]:
+		if not subscriber.is_valid():
+			continue
+		
+		# Call with or without etc
+		if etc == null:
+			subscriber.call()
+		else:
+			subscriber.call(etc)
+	return true
 
 static func unsubscribe(from: String, subscriber: Callable) -> bool:
 	if from in subscribed_events and subscribed_events[from] is Array:
