8edac4a6a3af90105cccd214be16ff082be3bf95
Squabix <102375958+squabix@users.noreply.github.com>
2026-05-20 21:33:55 -0600

    Clean up docking manager

--- island/docking/docking_manager.gd
+++ island/docking/docking_manager.gd
@@ -4,33 +4,48 @@ const DOCK_ELEVATION_OFFSET := 0.65
 const DEFAULT_DOCK_PLACE_RAY_LENGTH := 400.0
 const DOCK_EXPOSED_LENGTH := 6.0
 
-@onready var dock_place_ray_container: Node3D = $DockPlaceRayContainer
-@onready var dock: Node3D = $Dock
-@onready var boat: Boat = $Boat
+@export var dock_place_ray_container: Node3D
+@export var dock: Node3D
+@export var boat: Boat
 
 func _ready() -> void:
 	await get_tree().process_frame
 	place_dock()
 	boat.dock_position = $Dock/BoatDockPoint.global_position
 
+func extend_dock_place_rays() -> void:
+	for ray in dock_place_ray_container.get_children():
+		ray.target_position = Vector3.FORWARD * DEFAULT_DOCK_PLACE_RAY_LENGTH
+
+func get_first_ray_collision_point() -> Vector3:
+	return (dock_place_ray_container.get_child(0) as RayCast3D).get_collision_point()
+
+func set_dock_position(to: Vector3) -> void:
+	dock.global_position = to + Vector3.UP * DOCK_ELEVATION_OFFSET
+	dock.global_position = dock.global_position.move_toward(global_position, -DOCK_EXPOSED_LENGTH)
+
+func are_dock_places_rays_colliding(at_point: Vector3) -> bool:
+	var length := dock_place_ray_container.global_position.distance_to(at_point) - DEFAULT_DOCK_PLACE_RAY_LENGTH
+		
+	for ray in dock_place_ray_container.get_children():
+		ray.target_position = Vector3.FORWARD * length
+		ray.force_raycast_update()
+		if not ray.is_colliding():
+			return false
+	
+	return true
+
 func place_dock() -> void:
 	var found_placement := false
+	
+	var placement_point: Vector3
+	
 	while not found_placement:
+		extend_dock_place_rays()
 		rotation_degrees.y = randf_range(0.0, 360.0)
 		found_placement = true
-		for ray in dock_place_ray_container.get_children():
-			ray.target_position = Vector3.FORWARD * DEFAULT_DOCK_PLACE_RAY_LENGTH
 		
-		var first_point := (dock_place_ray_container.get_child(0) as RayCast3D).get_collision_point()
-		var first_length := dock_place_ray_container.global_position.distance_to(first_point) - 400.0
-		
-		for ray in dock_place_ray_container.get_children():
-			ray.target_position = Vector3.FORWARD * first_length
-			ray.force_raycast_update()
-			if not ray.is_colliding():
-				found_placement = false
-				break
+		placement_point = get_first_ray_collision_point()
+		found_placement = are_dock_places_rays_colliding(placement_point)
 	
-	dock.global_position = dock_place_ray_container.get_child(0).get_collision_point()
-	dock.global_position += Vector3.UP * DOCK_ELEVATION_OFFSET
-	dock.global_position = dock.global_position.move_toward(global_position, -DOCK_EXPOSED_LENGTH)
+	set_dock_position(placement_point)
