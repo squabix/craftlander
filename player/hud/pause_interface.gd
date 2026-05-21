cbd9839f4291e78cb9a1861103df9d4330c90f68
Squabix <102375958+squabix@users.noreply.github.com>
2026-05-21 17:11:14 -0600

    Change full screen interface to pause interface

---
+++ player/hud/pause_interface.gd
@@ -0,0 +1,48 @@
+extends Control
+
+signal updated_pause
+
+const ACTION_PAUSE := "ui_cancel"
+
+@export var crafting_menu: Control
+@export var crafting_environment: CraftingEnvironment
+@export var settings_menu: Menu
+@export var settings_button: Button
+@export var health: Health
+
+var is_paused := false
+var can_update_pause := true
+
+func _ready() -> void:
+	update()
+	health.died.connect(disable_update_pause)
+	health.revived.connect(enable_update_pause)
+	settings_button.pressed.connect(open_settings)
+
+func open_settings() -> void:
+	crafting_menu.hide()
+	settings_menu.show()
+	await settings_menu.backed_out
+	settings_menu.hide()
+	crafting_menu.show()
+
+func enable_update_pause() -> void: can_update_pause = true
+func disable_update_pause() -> void: can_update_pause = false
+
+func update() -> void:
+	visible = is_paused
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE if is_paused else Input.MOUSE_MODE_CAPTURED
+
+func pressed_pause() -> bool:
+	return Input.is_action_just_pressed(ACTION_PAUSE)
+
+func toggle_pause() -> void:
+	is_paused = !is_paused
+	get_tree().paused = is_paused
+	crafting_environment.is_crafting = is_paused
+	update()
+	updated_pause.emit()
+
+func _process(_delta: float) -> void:
+	if can_update_pause and pressed_pause():
+		toggle_pause()
