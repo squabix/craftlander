ae71bbe05dd234b199511c951ad68f541dce68b2
Squabix <102375958+squabix@users.noreply.github.com>
2026-05-22 16:58:13 -0600

    Clean up item pickup

--- common3d/inventory/item_pickup3d.gd
+++ common3d/inventory/item_pickup3d.gd
@@ -19,14 +19,17 @@ static func from_item(_item: Item) -> ItemPickup3D:
 	pickup.item = _item
 	return pickup
 
-func _ready() -> void:
-	if item == null:
-		printerr(self, " has no item instance")
-		return
+func update_visuals() -> void:
 	visuals = item.get_visuals_duplicate()
 	add_child(visuals)
 	visuals.global_position = self.global_position
 	visuals.global_rotation = self.global_rotation
+
+func _ready() -> void:
+	if item == null:
+		printerr(self, " has no item instance")
+		return
+	update_visuals()
 	
 	enabled_tooltip = tooltip_prefix + item.name + tooltip_suffix
 	
@@ -34,25 +37,26 @@ func _ready() -> void:
 		Util.snap_to_floor(self, FLOOR_MARGIN)
 	
 	if auto_generate_collision:
-		generate_collision()
+		generate_all_collision()
 
-func generate_collision() -> Array[CollisionShape3D]:
+func generate_all_collision() -> Array[CollisionShape3D]:
 	var collision_shapes: Array[CollisionShape3D] = []
 	var mesh_instances := Util.find_children_of_class(visuals, "MeshInstance3D")
 	
-	for mesh_instance in mesh_instances:
+	for mesh_instance: MeshInstance3D in mesh_instances:
 		if mesh_instance.mesh == null:
 			continue
-		
-		var collision_shape := CollisionShape3D.new()
-		collision_shape.shape = mesh_instance.mesh.create_convex_shape()
-		add_child.call_deferred(collision_shape)
-		collision_shape.global_transform = mesh_instance.global_transform
-		#collision_shape.scale = mesh_instance.scale * visuals.scale * collision_scale
-		collision_shapes.append(collision_shape)
-		
+		collision_shapes.append(add_collision_shape(mesh_instance))
 	
 	return collision_shapes
+
+func add_collision_shape(mesh_instance: MeshInstance3D) -> CollisionShape3D:
+	var collision_shape := CollisionShape3D.new()
+	collision_shape.shape = mesh_instance.mesh.create_convex_shape()
+	add_child.call_deferred(collision_shape)
+	collision_shape.global_transform = mesh_instance.global_transform
+	#collision_shape.scale = mesh_instance.scale * visuals.scale * collision_scale
+	return collision_shape
 
 func interact(_source: Node, _etc: Dictionary={}) -> void:
 	var inventory: Inventory = Util.find_child_of_class(_source, "Inventory")
