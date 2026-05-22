ff0f322bd6eb3ddd61c47f63707c23f2b2d04051
Squabix <102375958+squabix@users.noreply.github.com>
2026-05-22 16:28:48 -0600

    Clean up item animation tree

--- common/inventory_system/animation/item_animation_tree.gd
+++ common/inventory_system/animation/item_animation_tree.gd
@@ -18,73 +18,78 @@ class_name ItemAnimationTree
 @export var continue_use_state := "continue_use"
 @export var end_use_state := "end_use"
 
-var item: Item
+var current_item: Item
 var start_anim := ""
 var continue_anim := ""
 var end_anim := ""
 var playback: AnimationNodeStateMachinePlayback
 
 func _ready() -> void:
-	default()
+	default_animations()
 	
 	playback = get(state_machine_path + "/playback")
 	if playback != null:
 		
-		# Disable item blend when item state machine finishes
+		# Disable current item blend when item state machine finishes
 		playback.state_finished.connect(
 			func(state_name: String) -> void:
 				if state_name == end_use_state:
 					disable_item_blend()
-				elif item != null and item.current_use_state == Item.UseState.END_USE:
+				elif current_item != null and current_item.current_use_state == Item.UseState.END_USE:
 					play_end()
 		)
 	disable_item_blend()
-	active = true 
+	
+	active = true
 
-func default() -> void:
+func default_animations() -> void:
 	start_anim = default_start_anim
 	continue_anim = default_continue_anim
 	end_anim = default_end_anim
-	_assign_nodes_to_tree()
 
 func update_item(new_item: Item) -> void:
 	
 	# Wait until item is unique
-	if not new_item.is_unique:
+	if new_item != null and not new_item.is_unique:
 		await new_item.made_unique
 	
 	# Disconnect old item's signals
-	if item != null:
-		item.started_use.disconnect(play_start)
-		item = null
+	if current_item != null:
+		current_item.started_use.disconnect(play_start)
+		current_item = null
 	
-	var anims: ItemAnimations = null
+	if new_item == null:
+		reset_current_item()
+		return
 	
-	# Find new item animations
-	for item_anims in item_animations:
-		var key = new_item.find_in_array(item_anims.items)
-		if key != null:
-			anims = item_anims
-			break
+	var anims: ItemAnimations = get_animations(new_item)
 	
-	# Update to new animations
-	if anims != null:
-		item = new_item
-		
-		# Validate animations exist in the AnimationPlayer
-		start_anim = anims.start_anim if has_animation(anims.start_anim) else ""
-		continue_anim = anims.continue_anim if has_animation(anims.continue_anim) else ""
-		end_anim = anims.end_anim if has_animation(anims.end_anim) else ""
-		
-		_assign_nodes_to_tree()
-	
-	# Default if new animations are null
-	else:
-		default()
+	load_animations(anims)
+	update_tree_animations()
 	
 	# Connect new item's signals
-	if item != null:
-		item.started_use.connect(play_start)
+	new_item.started_use.connect(play_start)
+	
+	current_item = new_item
+
+func reset_current_item() -> void:
+	current_item.started_use.disconnect(play_start)
+	current_item = null
+
+func get_animations(item: Item) -> ItemAnimations:
+	for item_anims in item_animations:
+		var key := item.find_in_array(item_anims.items)
+		if key != null:
+			return item_anims
+	return null
+
+func load_animations(anims: ItemAnimations) -> void:
+	if anims == null:
+		default_animations()
+		return
+	start_anim = anims.start_anim if has_animation(anims.start_anim) else ""
+	continue_anim = anims.continue_anim if has_animation(anims.continue_anim) else ""
+	end_anim = anims.end_anim if has_animation(anims.end_anim) else ""
 
 func get_player() -> AnimationPlayer:
 	return get_node(anim_player)
@@ -115,11 +120,14 @@ func play_end() -> void:
 	if end_anim.is_empty():
 		disable_item_blend()
 
-func _assign_nodes_to_tree() -> void:
-	var state_anim_map: Dictionary[String, String] = {
+func get_state_anim_map() -> Dictionary[String, String]:
+	return {
 		start_use_state: start_anim,
 		continue_use_state: continue_anim,
 		end_use_state: end_anim
 	}
+
+func update_tree_animations() -> void:
+	var state_anim_map: Dictionary[String, String] = get_state_anim_map()
 	for state in state_anim_map:
 		set(state_machine_path + "/" + state + "/animation", state_anim_map[state])
