1b15912e6ca0d68465091f7753943453ee7f5886
Squabix <102375958+squabix@users.noreply.github.com>
2026-05-22 19:10:06 -0600

    Clean up item visuals container

--- common3d/item_visuals_container_3d.gd
+++ common3d/item_visuals_container_3d.gd
@@ -9,7 +9,7 @@ const GHOSTED_ANIMATION_PROPERTIES: PackedStringArray = [
 @export var item_holder: ItemHolder3D
 @export var inventory_holder_link: InventoryHolderLink
 @export var visuals_scale_ratio := 1.0
-@export var disable_shadows := false
+@export var do_disable_shadows := false
 
 var instance: ItemInstance
 var contained_visuals: Node3D
@@ -27,40 +27,44 @@ func reset_visuals() -> void:
 	for child in get_children():
 		Util.safe_free(child)
 
+func ensure_item_visuals() -> bool:
+	if instance.item.is_scene_set_up:
+		return true
+	await instance.item.scene_set_up
+	return instance.item.visuals != null
+
 func update_visuals() -> void:
 	reset_visuals()
 	instance = item_holder.item_instance
 	
 	# Cannot update visuals if no instance/item
-	if instance == null:
+	if instance == null or instance.item == null:
 		return
-	if instance.item == null:
-		return
-	# if not instance in inventory_holder_link.inventory.item_instances:
-	# 	return
+	
 	# Free current visuals
 	Util.safe_free(contained_visuals)
 	
 	# Wait for visuals to be set when item's scene is set up
-	if not instance.item.is_scene_set_up:
-		await instance.item.scene_set_up
-		
-		# Return if visuals are still not set
-		if instance.item.visuals == null:
-			return
+	if not await ensure_item_visuals():
+		return
 	
 	# Ghost a duplicate of visuals to copy animation
 	contained_visuals = instance.item.visuals.duplicate()
 	add_child(contained_visuals)
-	contained_visuals.show()
 	Util.ghost(
 		instance.item.visuals,
 		contained_visuals,
 		GHOSTED_ANIMATION_PROPERTIES,
 		get_tree().process_frame
 	)
-	if disable_shadows:
-		for mesh_instance in Util.find_children_of_class(contained_visuals, "MeshInstance3D"):
-			mesh_instance.cast_shadow = false
+	
+	contained_visuals.show()
+	if do_disable_shadows: disable_shadows()
+	
+	# Transform contained visuals
 	contained_visuals.position = Vector3.ZERO
 	contained_visuals.scale *= visuals_scale_ratio
+
+func disable_shadows() -> void:
+	for mesh_instance in Util.find_children_of_class(contained_visuals, "MeshInstance3D"):
+		mesh_instance.cast_shadow = false
