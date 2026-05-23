f17aaf3fafc8a342325c1928716433a1f6673608
Squabix <102375958+squabix@users.noreply.github.com>
2026-05-22 20:28:40 -0600

    Clean up nav entity guide

--- common3d/entity/entity_guide/nav_entity_guide3d.gd
+++ common3d/entity/entity_guide/nav_entity_guide3d.gd
@@ -8,13 +8,15 @@ func set_target(to: Vector3) -> void:
 	nav.target_position = to
 
 func get_direction() -> Vector3:
-	if not is_instance_valid(entity):
+	if not has_entity():
 		return Vector3.ZERO
 	var next: Vector3 = nav.get_next_path_position()
 	return entity.global_position.direction_to(next)
 
+func has_entity() -> bool: return is_instance_valid(entity)
+
 func face_target() -> void:
-	if not is_instance_valid(entity):
+	if not has_entity():
 		return
 	if not nav.is_target_reachable():
 		Util.lerp_look_at_3d(entity, target_position, face_interpolation)
@@ -31,30 +33,36 @@ func move_forward() -> void:
 		return
 	entity.move_forward()
 
-func get_nearby_navigable_position(
-	inner_radius: float,
-	outer_radius: float,
-	attempts: int = 16
-) -> Vector3:
+func get_annulus_point(inner_radius: float, outer_radius: float) -> Vector3:
+	if not has_entity():
+		printerr(self, " cannot get annulus point without entity")
+		return Vector3.ZERO
+	
+	var angle := randf() * TAU
+	var radius := sqrt(randf_range(inner_radius * inner_radius, outer_radius * outer_radius))
+	return entity.global_position + Vector3(cos(angle) * radius, 0.0, sin(angle) * radius)
+
+func get_nearby_navigable_position(inner_radius: float, outer_radius: float, attempts: int = 16) -> Vector3:
+	if not has_entity():
+		printerr(self, " cannot find navigable position without entity")
+		return Vector3.ZERO
+	
 	var nav_map := nav.get_navigation_map()
-	if nav_map == RID():
-		return entity.global_position
+	var origin := entity.global_position
+	
+	# Fail to navigate if no RID
+	if not nav_map.is_valid():
+		return origin
 	
 	inner_radius = max(inner_radius, 0.0)
 	outer_radius = max(outer_radius, inner_radius)
+	attempts = max(attempts, 1)
 	
 	for i in attempts:
 		# Random point in an annulus on the XZ plane
-		var angle := randf() * TAU
-		var dist: float = lerp(inner_radius, outer_radius, randf())
-		var candidate := entity.global_position + Vector3(cos(angle) * dist, 0.0, sin(angle) * dist)
-	
-		var nav_point := NavigationServer3D.map_get_closest_point(nav_map, candidate)
-	
-		var d := nav_point.distance_to(entity.global_position)
-		if d >= inner_radius and d <= outer_radius:
+		var nav_point := NavigationServer3D.map_get_closest_point(nav_map, get_annulus_point(inner_radius, outer_radius))
+		var distance := nav_point.distance_to(origin)
+		if distance >= inner_radius and distance <= outer_radius:
 			return nav_point
 	
-	# Fallback: closest point to the middle of the ring
-	var fallback_pos := entity.global_position + Vector3(outer_radius, 0.0, 0.0)
-	return NavigationServer3D.map_get_closest_point(nav_map, fallback_pos)
+	return entity.global_position
