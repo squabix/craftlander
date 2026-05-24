4f3500b8991c67f2e8eda27f58e58e587c672bef
Squabix <102375958+squabix@users.noreply.github.com>
2026-05-23 23:07:15 -0600

    Create mirror state machine

---
+++ common/state_machine/mirror_state_machine.gd
@@ -0,0 +1,30 @@
+extends StateMachine
+class_name MirrorStateMachine
+
+@export var initial_target: StateMachine
+@export var do_find_root_machine := true
+
+var target: StateMachine
+
+func enter() -> void:
+	if do_find_root_machine:
+		set_target(Util.find_child_of_class(root, "StateMachine"))
+		match_state(target.current)
+
+func _ready() -> void:
+	super()
+	if initial_target:
+		set_target(initial_target)
+	await get_tree().process_frame
+	if target:
+		match_state(target.current)
+
+func match_state(state: State) -> void:
+	enter_state(str(state))
+
+func set_target(to: StateMachine) -> void:
+	if target != null:
+		target.entered_state.disconnect(match_state)
+	target = to
+	if target != null:
+		target.entered_state.connect(match_state)
