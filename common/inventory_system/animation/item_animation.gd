82c72d5e7c1df1183b6063861c3afbf2ecb7f9e2
Squabix <102375958+squabix@users.noreply.github.com>
2026-06-29 13:25:26 -0600

    Add to string method for item animation

--- common/inventory_system/animation/item_animation.gd
+++ common/inventory_system/animation/item_animation.gd
@@ -5,3 +5,18 @@ class_name ItemAnimations
 @export var start_anim := ""
 @export var continue_anim := ""
 @export var end_anim := ""
+
+func _to_string() -> String:
+	if start_anim.is_empty() and continue_anim.is_empty() and end_anim.is_empty():
+		return "ItemAnimations (empty)"
+	
+	var s := "ItemAnimations ("
+	if not start_anim.is_empty():
+		s += start_anim
+	if not continue_anim.is_empty():
+		s += " → %s" %continue_anim
+	if not end_anim.is_empty():
+		s += " → %s" %end_anim
+	s += ")"
+	
+	return s
