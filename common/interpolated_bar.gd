cbf136aab69ba0928443988cf9d5595048bf74de
Squabix <102375958+squabix@users.noreply.github.com>
2026-07-01 21:58:13 -0600

    Add initial fade in suspension to interpolated bar
    If fade_when_idle is set to true, an InterpolatedBar now can wait a certain amount of time after ready before fading in to prevent being immediately visible.

--- common/interpolated_bar.gd
+++ common/interpolated_bar.gd
@@ -10,30 +10,28 @@ extends ProgressBar
 @export_custom(PROPERTY_HINT_NONE, "suffix:s") var idle_fade_wait_time := 2.0
 @export_custom(PROPERTY_HINT_NONE, "suffix:s") var fade_in_duration := 0.1
 @export_custom(PROPERTY_HINT_NONE, "suffix:s") var fade_out_duration := 0.1
+@export_custom(PROPERTY_HINT_NONE, "suffix:s") var initial_fade_in_suspension := 0.1
 @export var fade_target_override: CanvasItem
 
 var target_value := value:
 	set(to):
-		if is_equal_approx(target_value, to):
+		if is_equal_approx(target_value, to) or not passed_initial_fade_in_suspension() or not fade_when_idle:
 			target_value = to
 			return
-		
+
 		target_value = to
-		
-		if not fade_when_idle:
-			return
-		
-		# Kill any active fade-out or fade-in tweens
-		if _fade_tween and _fade_tween.is_valid():
+
+		# Kill active fade tween
+		if _fade_tween != null and _fade_tween.is_valid():
 			_fade_tween.kill()
-		
+
 		# Smoothly fade in to 1.0 alpha
 		_fade_tween = create_tween()
 		_fade_tween.tween_property(fade_target, "modulate:a", 1.0, fade_in_duration)
-		
+
 		# Create/Reset the idle timer
 		_idle_timer = get_tree().create_timer(idle_fade_wait_time)
-		
+
 		# Connect it to the fade function (using a lambda)
 		var current_timer := _idle_timer
 		_idle_timer.timeout.connect(
@@ -44,6 +42,7 @@ var target_value := value:
 var fade_target: CanvasItem
 var _idle_timer: SceneTreeTimer
 var _fade_tween: Tween
+var _initial_time: float
 
 
 func _ready() -> void:
@@ -51,6 +50,7 @@ func _ready() -> void:
 	target_value = value
 	if fade_when_idle:
 		fade_target.modulate.a = 0.0
+	_initial_time = Util.get_time_seconds()
 
 
 func _process(_delta: float) -> void:
@@ -58,6 +58,10 @@ func _process(_delta: float) -> void:
 	var distance: float = abs(value - target_value)
 	if distance <= jump_distance:
 		value = target_value
+
+
+func passed_initial_fade_in_suspension() -> bool:
+	return Util.get_time_seconds() - _initial_time >= initial_fade_in_suspension
 
 
 func _start_fade_out() -> void:
