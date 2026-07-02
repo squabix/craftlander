979016ad4c7bf88fe9121e3f734b2e91a679991f
Squabix <102375958+squabix@users.noreply.github.com>
2026-07-01 20:54:55 -0600

    Implement entity health bar
    Implement Unprojector3D class for unprojecting CanvasItems to screen via current viewport camera. Add instantiable scene for entity health bar. Add entity health bar to Rootling and Bee scenes.

---
+++ common3d/unprojector_3d.gd
@@ -0,0 +1,63 @@
+class_name Unprojector3D
+extends Marker3D
+
+@export var target: CanvasItem
+@export_group("Offsets")
+@export var constant_offset := Vector2(0.0, 0.0)
+@export var control_size_ratio_offset := Vector2(-0.5, -0.5)
+
+@export_group("Distance Scaling")
+## The distance (in meters) from the camera where the UI element is at its default scale (1.0).
+@export_custom(PROPERTY_HINT_NONE, "suffix:m") var reference_distance := 10.0
+## Controls how much it scales with distance. 
+## 1.0 is realistic perspective. 0.0 keeps it a completely flat, constant size.
+@export_range(0.0, 1.0) var scale_intensity := 1.0
+## The maximum scale limit when very close to the camera.
+@export_custom(PROPERTY_HINT_NONE, "suffix:m") var max_scale := 1.0
+## The minimum scale limit so it doesn't shrink into invisible pixels far away.
+@export_custom(PROPERTY_HINT_NONE, "suffix:m") var min_scale := 0.25
+
+var camera: Camera3D
+
+
+func _ready() -> void:
+	camera = get_viewport().get_camera_3d()
+
+
+func _process(_delta: float) -> void:
+	if not is_instance_valid(camera):
+		printerr(name, " cannot unproject with invalid camera")
+		set_process(false)
+		return
+
+	if not is_instance_valid(target):
+		return
+
+	# Hide target if behind camera
+	if camera.is_position_behind(global_position):
+		target.visible = false
+		return
+	
+	target.visible = true
+	
+	# Scale the target node
+	if scale_intensity > 0.0:
+		target.scale = get_distance_scale()
+	
+	# Position the target on screen
+	var screen_pos = camera.unproject_position(global_position)
+	target.position = screen_pos + constant_offset
+	
+	if target is Control:
+		target.position += target.size * target.scale * control_size_ratio_offset
+
+
+func get_distance_scale() -> Vector2:
+	var camera_distance := camera.global_position.distance_to(global_position)
+	if camera_distance < 0.01: 
+		camera_distance = 0.01
+		
+	var raw_projected_scale := reference_distance / camera_distance
+	
+	# Lerp between between flat scale (1.0) and perspective scale based on intensity, clamped to min/max limits
+	return Vector2.ONE * clampf(lerpf(1.0, raw_projected_scale, scale_intensity), min_scale, max_scale)
---
+++ entities/entity_health_bar.gd
@@ -0,0 +1,13 @@
+extends Unprojector3D
+
+@export var health_bar: HealthBar
+@export var type_label: Label
+@export_group("External Dependencies")
+@export var entity: Entity3D
+@export var health: Health
+
+func _ready() -> void:
+	super()
+	health_bar.health = health
+	if is_instance_valid(entity) and is_instance_valid(type_label):
+		type_label.text = entity.type
