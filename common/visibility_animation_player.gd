777495bee71ac0420b381b16fe1c48cc00a45818
Squabix <102375958+squabix@users.noreply.github.com>
2026-07-06 16:44:39 -0600

    Implement VisibilityAnimationPlayer
    Implement VisibilityAnimationPlayer, which shows animated nodes when they are being animated and hides animated nodes when they are not. Useful for integration with MeshInstanceAggregator3D and its invert_instance_visibility property.

---
+++ common/visibility_animation_player.gd
@@ -0,0 +1,48 @@
+class_name VisibilityAnimationPlayer
+extends AnimationPlayer
+
+@export var show_on_play := true
+@export var hide_on_finish := true
+
+var _affected_nodes: Array[Node] = []
+
+
+func _ready() -> void:
+	animation_started.connect(_on_animation_started)
+	animation_finished.connect(_on_animation_finished)
+
+
+func is_affectable(node: Node) -> bool:
+	return is_instance_valid(node) and node.has_method("show") and node.has_method("hide")
+
+
+func get_track_node(path: NodePath) -> Node:
+	return get_node(root_node).get_node(str(path).split(":")[0])
+
+
+func _on_animation_started(anim_name: StringName) -> void:
+	if not show_on_play:
+		return
+
+	_affected_nodes.clear()
+	var anim: Animation = get_animation(anim_name)
+
+	for i in anim.get_track_count():
+		var target_node: Node = get_track_node(anim.track_get_path(i))
+		if not is_affectable(target_node) or _affected_nodes.has(target_node):
+			continue
+
+		_affected_nodes.append(target_node)
+		target_node.show()
+
+
+func _on_animation_finished(_anim_name: StringName) -> void:
+	if not hide_on_finish:
+		return
+
+	for node in _affected_nodes:
+		if not is_instance_valid(node):
+			continue
+		node.hide()
+
+	_affected_nodes.clear()
