1836e48d05832a23fdaa5aaa37d697f59137320d
Squabix <102375958+squabix@users.noreply.github.com>
2026-07-07 12:18:32 -0600

    Update IslandNavRegion baking
    Remove bake_on_ready export variable (IslandNavRegion will now always bake after island is populated) and add island_navigation_baked EventBus event.

--- island/island_nav_region.gd
+++ island/island_nav_region.gd
@@ -2,17 +2,16 @@ extends NavigationRegion3D
 class_name IslandNavRegion
 
 @export var island_generator: HeightmapTerrainGenerator
-@export var bake_on_ready := true
 
 static var current: IslandNavRegion
 
 func _ready() -> void:
 	current = self
-	if bake_on_ready:
-		EventBus.subscribe("island_populated", bake)
+	EventBus.subscribe("island_populated", bake)
 
 func bake() -> void:
 	get_tree().paused = true
 	bake_navigation_mesh(true)
 	await bake_finished
 	get_tree().paused = false
+	EventBus.trigger("island_navigation_baked")
