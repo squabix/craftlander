6d6c9ac941d6b8cc990732548c6ea6e44ae7c4b6
Squabix <102375958+squabix@users.noreply.github.com>
2026-07-08 19:15:13 -0600

    Integrate IntervalStaggerer with Chasing state
    Integrate IntervalStaggerer with Rootling and Bee Chasing state

--- entities/npc_states/chasing_state.gd
+++ entities/npc_states/chasing_state.gd
@@ -1,45 +1,33 @@
 extends State
 
-const STAGGER_RANDOMNESS := 0.03
-
 @export var item_holder: ItemHolder3D
 @export var guide: EntityGuide3D
 @export var sight: RadialSight3D
+@export var interval_staggerer: IntervalStaggerer
 @export var default_target: Node3D
 @export_custom(PROPERTY_HINT_NONE, "m") var max_item_use_distance := 1.5
-@export_custom(PROPERTY_HINT_NONE, "s") var path_update_interval := 0.25
 
 @export_group("Target Losing")
 @export var can_lose_target := true
 @export var lose_target_state := ""
 
-var _path_update_time_counter := 0.0
+
+func _ready() -> void:
+	interval_staggerer.disabled = not is_active
 
 
 func enter() -> void:
-	# Stagger the initial timer so multiple enemies don't tick on the exact same frame
-	_path_update_time_counter = randf_range(0.0, path_update_interval)
+	interval_staggerer.disabled = false
 
 
-func reset_path_update_time_counter() -> void:
-	_path_update_time_counter = path_update_interval + randf_range(-STAGGER_RANDOMNESS, +STAGGER_RANDOMNESS)
+func exit() -> void:
+	interval_staggerer.disabled = true
 
 
 func physics_update(_delta: float) -> void:
 	if not is_instance_valid(guide):
 		printerr("Chasing state of %s has no guide" % root)
 		return
-
-	_path_update_time_counter -= _delta
-	if _path_update_time_counter <= 0.0:
-		# Reset timer with slight randomization to maintain stagger separation
-		reset_path_update_time_counter()
-
-		if can_lose_target and not can_see_target():
-			transition_to(lose_target_state)
-			return
-
-		guide.set_target(get_target_position())
 
 	guide.face_target()
 
@@ -51,6 +39,13 @@ func physics_update(_delta: float) -> void:
 	# Move forward to get in range
 	else:
 		guide.move_forward()
+
+
+func update_path() -> void:
+	if can_lose_target and not can_see_target():
+		transition_to(lose_target_state)
+		return
+	guide.set_target(get_target_position())
 
 
 func can_see_target() -> bool:
