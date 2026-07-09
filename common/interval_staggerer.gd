5392f7fe2ba0fa008af7fea32833156f5bab6e26
Squabix <102375958+squabix@users.noreply.github.com>
2026-07-08 21:09:38 -0600

    Fix item pickup processing
    Update default rigid item pickups to have a larger on screen processing radius. Export initial_time_left from IntervalStaggerer for a longer initial time left (settling period for rigid item pickups).

--- common/interval_staggerer.gd
+++ common/interval_staggerer.gd
@@ -5,29 +5,19 @@ signal timeout
 
 @export var disabled := false
 @export_custom(PROPERTY_HINT_NONE, "suffix:s") var base_interval := 1.0
-@export_custom(PROPERTY_HINT_NONE, "suffix:s") var stagger := 0.003
+@export_custom(PROPERTY_HINT_NONE, "suffix:s") var stagger_amount := 0.003
 @export var target_methods: Dictionary[Node, StringName]
+@export_custom(PROPERTY_HINT_NONE, "suffix:s") var initial_time_left := 0.0
 
 var time_left := 0.0
 
+
 func _ready() -> void:
+	if initial_time_left > 0.0:
+		time_left = stagger(initial_time_left)
+		return
 	reset()
 
-func reset() -> void:
-	time_left = base_interval + randf_range(-stagger, stagger)
-
-func call_target_methods() -> void:
-	if disabled:
-		return
-	
-	for node in target_methods.keys():
-		if not is_instance_valid(node):
-			continue
-		
-		var method_name: StringName = target_methods[node]
-		if not node.has_method(method_name):
-			continue
-		node.call(method_name)
 
 func _process(delta: float) -> void:
 	time_left -= delta
@@ -35,3 +25,25 @@ func _process(delta: float) -> void:
 		reset()
 		call_target_methods()
 		timeout.emit()
+
+
+func reset() -> void:
+	time_left = stagger(base_interval)
+
+
+func stagger(value: float) -> float:
+	return value + randf_range(-stagger_amount, stagger_amount)
+
+
+func call_target_methods() -> void:
+	if disabled:
+		return
+
+	for node in target_methods.keys():
+		if not is_instance_valid(node):
+			continue
+
+		var method_name: StringName = target_methods[node]
+		if not node.has_method(method_name):
+			continue
+		node.call(method_name)
