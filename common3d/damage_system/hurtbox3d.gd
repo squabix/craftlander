869c20ded418865543518b61f2129f3f6f8aba38
Squabix <102375958+squabix@users.noreply.github.com>
2026-07-10 12:17:17 -0600

    Reorganize Hurtbox3D
    Reorganize export variables. Integrate new override function in Damage class into damage overriding.

--- common3d/damage_system/hurtbox3d.gd
+++ common3d/damage_system/hurtbox3d.gd
@@ -6,23 +6,26 @@ signal was_dealt_damage(damage: Damage)
 
 @export var health: Health
 @export var inactive := false
-@export var free_parent_on_hurt := false
 @export var damage_multiplier := 1.0
-@export var center := Vector3.ZERO
 @export var type_whitelist: Array[String] = []
-@export var damage_override: Damage
+@export var free_parent_on_hurt := false
 
-@export_group("Knockback")
+@export_group("Damage Override")
+@export var damage_override: Damage
+@export var override_null := false
+
+@export_group("Knockback", "knockback")
 @export var knockback_entity: Entity3D
 @export var knockback_multiplier := 1.0
+@export_custom(PROPERTY_HINT_NONE, "suffix:m") var knockback_center_offset := Vector3.ZERO
 
-@export_group("Auto Hurt")
-@export var bodies_auto_hurt := false
-@export var areas_auto_hurt := false
+@export_group("Auto Hurt", "auto_hurt")
 @export var auto_hurt_damage: Damage
+@export var auto_hurt_bodies_enabled := false
+@export var auto_hurt_areas_enabled := false
 
-var last_hurt_direction: Vector3
-var total_damage_taken: float
+var last_hurt_direction := Vector3.ZERO
+var total_damage_taken := 0.0
 
 
 func _ready() -> void:
@@ -30,11 +33,12 @@ func _ready() -> void:
 
 
 func auto_hurt(node: Node3D = null) -> void:
-	hurt(auto_hurt_damage, node.global_position)
+	var direction := get_hurt_direction_from(node.global_position) if is_instance_valid(node) else Vector3.ZERO
+	hurt(auto_hurt_damage, direction)
 
 
 func get_hurt_direction_from(from_position: Vector3) -> Vector3:
-	return from_position.direction_to(global_position + center)
+	return from_position.direction_to(global_position + knockback_center_offset)
 
 
 func is_type_whitelisted(type: String) -> bool:
@@ -55,17 +59,14 @@ func hurt(damage: Damage, direction: Vector3 = Vector3.ZERO) -> float:
 	if inactive:
 		return 0.0
 
-	if damage_override != null:
-		# Override damage
-		var temp: Damage = damage_override.duplicate()
-		temp.source = damage.source
-		damage = temp
-	else:
-		# Confirm damage is valid
-		if damage == null:
-			return 0.0
-		if not is_type_whitelisted(damage.type):
-			return 0.0
+	if damage_override != null and (damage != null or override_null):
+		damage = damage_override.override(damage)
+	
+	# Confirm damage is valid
+	if damage == null:
+		return 0.0
+	if not is_type_whitelisted(damage.type):
+		return 0.0
 
 	var damage_amount := apply_damage_amount(damage.sample())
 
@@ -89,7 +90,7 @@ func knock(direction: Vector3, base_force: float) -> void:
 
 
 func _set_up_auto_hurt() -> void:
-	if bodies_auto_hurt:
+	if auto_hurt_bodies_enabled:
 		body_entered.connect(auto_hurt)
-	if areas_auto_hurt:
+	if auto_hurt_areas_enabled:
 		area_entered.connect(auto_hurt)
