9f39d92973e3fb70182baa7ba3d7c2f96ea96daa
Squabix <102375958+squabix@users.noreply.github.com>
2026-07-16 15:00:31 -0600

    Add connected check to player death screen quit

--- player/ui/player_death_screen.gd
+++ player/ui/player_death_screen.gd
@@ -8,4 +8,7 @@ func _ready() -> void:
 	%Health.died.connect(show)
 	%Health.revived.connect(hide)
 	
+	var trigger_quit := EventBus.trigger.bind(&"quit_to_title")
+	if quit_button.pressed.is_connected(trigger_quit):
+		return
 	quit_button.pressed.connect(EventBus.trigger.bind(&"quit_to_title"))
