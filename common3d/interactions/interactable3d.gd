25c1ccf8e0af90c019c0336e8eeff1d84ae150ee
Squabix <102375958+squabix@users.noreply.github.com>
2026-07-19 13:12:28 -0600

    Add interacted_with signal to Interactable3D
    Add interacted_with signal to Interactable3D, which will emit be default after the interact function is called.

--- common3d/interactions/interactable3d.gd
+++ common3d/interactions/interactable3d.gd
@@ -2,6 +2,7 @@ class_name Interactable3D
 extends Area3D
 
 @export var enabled := true
+signal interacted_with(source: Node)
 @export var channel := 0
 
 @export_group("Tooltips", "tooltip")
@@ -17,8 +18,8 @@ func disable() -> void:
 	enabled = false
 
 
-func interact(_source: Node, _etc: Dictionary = { }) -> void:
-	printerr("Interact")
+func interact(source: Node, _etc: Dictionary = { }) -> void:
+	interacted_with.emit(source)
 
 
 func get_tooltip() -> String:
