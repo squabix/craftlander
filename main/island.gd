dedbc2ad00816789863adc47bd3c160549b5b1e7
Squabix <102375958+squabix@users.noreply.github.com>
2026-07-20 11:59:14 -0600

    Reorganize island loading logic
    Initial loading and reloading are now broken into their own functions. Add position_player_at_spawn function.

--- main/island.gd
+++ main/island.gd
@@ -25,31 +25,36 @@ func _ready() -> void:
 	seed(Main.base_seed)
 	island_generator.generate()
 	
-	if not Main.loaded_save.is_current_level_generated():
-		await island_generator.generated
-		
-		docking_manager.initialize()
-		prop_populator.populate()
-		
-		await prop_populator.populated
-		Main.loaded_save.mark_current_level_as_generated()
-	else:
-		prop_populator.clear()
-		Main.loaded_save.add_dynamic_nodes(self)
-		await island_generator.generated
+	await (reload_save if Main.loaded_save.is_current_level_generated() else initial_save_load).call()
 	
 	NodeSaver.load_all()
 	
 	mesh_aggregator.aggregate()
-	
 	occluder_instance.generate()
 	nav_region.reset.call_deferred()
 	
+	position_player_at_spawn()
+	
+	get_tree().paused = false
+	MouseModeController.capture()
+
+func initial_save_load() -> void:
+	await island_generator.generated
+		
+	docking_manager.initialize()
+	prop_populator.populate()
+	
+	await prop_populator.populated
+	Main.loaded_save.mark_current_level_as_generated()
+
+func reload_save() -> void:
+	prop_populator.clear()
+	Main.loaded_save.add_dynamic_nodes(self)
+	await island_generator.generated
+
+func position_player_at_spawn() -> void:
 	match player_spawn_mode:
 		PlayerSpawnMode.BOAT:
 			docking_manager.boat.driver_seat.mount(player)
 		PlayerSpawnMode.ISLAND_CENTER:
 			player.global_position = Vector3(0.0, ISLAND_CENTER_SPAWN_HEIGHT, 0.0)
-	
-	get_tree().paused = false
-	MouseModeController.capture()
