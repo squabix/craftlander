5a1642323ace0cf0df80d7a12d44490b9c93a410
Squabix <102375958+squabix@users.noreply.github.com>
2026-08-01 22:23:09 -0600

    Add SignalAudioManagers
    Add SignalAudioManager and SignalAudioManager3D for connecting signals to sound effects.

---
+++ common/signal_audio_manager.gd
@@ -0,0 +1,17 @@
+class_name SignalAudioManager
+extends Node
+
+@export var signal_players: Dictionary[StringName, AudioStreamPlayer]
+
+func _ready() -> void:
+	for signal_name in signal_players:
+		if not get_parent().has_signal(signal_name):
+			Util.node_error("%s cannot play audio for nonexistant signal '%s'", self, signal_name)
+			continue
+		
+		var player := signal_players[signal_name]
+		if not is_instance_valid(player):
+			Util.node_error("%s cannot play audio for signal '%s' with invalid AudioStreamPlayer")
+			continue
+		
+		get_parent().connect(signal_name, player.play)
