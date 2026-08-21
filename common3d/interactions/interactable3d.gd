04f3293c65a71bc41da8dc6ae3d4b9636d862e3d
Squabix <102375958+squabix@users.noreply.github.com>
2026-08-20 23:06:20 -0600

    Add post interaction mode to Interactable3D
    Add post interaction mode to Interactable3D, with 3 settings: nothing, disable, free.

--- common3d/interactions/interactable3d.gd
+++ common3d/interactions/interactable3d.gd
@@ -1,6 +1,8 @@
 class_name Interactable3D
 extends Area3D
 
+enum PostInteractionMode {NOTHING, DISABLE, FREE}
+
 signal interacted_with(source: Node)
 
 @export var enabled := true:
@@ -10,6 +12,7 @@ signal interacted_with(source: Node)
 			visible = enabled
 @export var channel := 0
 @export var visible_as_enabled := false
+@export var post_interaction_mode := PostInteractionMode.NOTHING
 
 @export_group("Tooltips", "tooltip")
 @export var tooltip_enabled := ""
@@ -26,6 +29,13 @@ func disable() -> void:
 
 func interact(source: Node, _etc: Dictionary = { }) -> void:
 	interacted_with.emit(source)
+	match post_interaction_mode:
+		PostInteractionMode.NOTHING:
+			pass
+		PostInteractionMode.DISABLE:
+			disable()
+		PostInteractionMode.FREE:
+			queue_free()
 
 
 func get_tooltip() -> String:
