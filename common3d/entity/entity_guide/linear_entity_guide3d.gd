c8500d8def78eb5029096a336f7c0c08ddc991f9
Squabix <102375958+squabix@users.noreply.github.com>
2026-08-24 16:10:07 -0600

    Override missing functions to LinearEntityGuide3D
    Override missing face_target and get_distance_to_target LinearEntityGuide3D functions,

--- common3d/entity/entity_guide/linear_entity_guide3d.gd
+++ common3d/entity/entity_guide/linear_entity_guide3d.gd
@@ -2,6 +2,21 @@ class_name LinearEntityGuide3D
 extends EntityGuide3D
 
 
+func face_target() -> void:
+	var face_direction := entity.global_position.direction_to(target_position)
+	Util.lerp_look_at_3d(
+		entity,
+		entity.global_position + face_direction,
+		face_interpolation,
+	)
+
+
+func get_distance_to_target() -> float:
+	if not is_instance_valid(entity):
+		return INF
+	return entity.global_position.distance_to(target_position)
+
+
 func get_direction() -> Vector3:
 	if not is_instance_valid(entity):
 		return Vector3.ZERO
