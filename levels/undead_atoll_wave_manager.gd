7c76d93d2d6f961921ccc034c1d0f94fb3c151ce
Squabix <102375958+squabix@users.noreply.github.com>
2026-08-25 21:40:11 -0600

    Add start tracking to Undead Atoll Wave Manager
    Undead Atoll Wave Manager will now only start once.

--- levels/undead_atoll_wave_manager.gd
+++ levels/undead_atoll_wave_manager.gd
@@ -3,17 +3,22 @@ extends WaveSpawner3D
 @export var player: Player
 @export var ghost_docking_managers: Array[DockingManager]
 
+var started := false
+
 
 func _ready() -> void:
 	EventBus.subscribe(&"treasure_chest_opened", start)
 
 
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("drop"):
+	if Input.is_action_just_pressed("drop") and not started:
 		EventBus.trigger(&"treasure_chest_opened")
 
 
 func start(start_index := 0) -> void:
+	if started:
+		return
+	started = true
 	print("Starting Undead Atoll waves")
 	for manager in ghost_docking_managers:
 		manager.add_boat()
